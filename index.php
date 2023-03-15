cceec3fab1aaae07ef65ff1a023c20e5eda494e5
Menno Engels <menno.engels@live.nl>
2023-03-16 00:44:59 +0100

    General outline and some content

---
+++ index.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+    <title>Resume | Menno Engels</title>
+    <meta charset="UTF-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>Menno Engels | Software Engineer</title>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+    <div class="center">
+        <div class="center-inner">
+            <div class="introduction">
+                <h1 id="intro_title" class="intro_title">Menno Engels</h1>
+                <h2 id="intro_sub" class="intro_sub">Software Engineer</h2>
+                <div class="content">
+                    <p id="intro_1" class="intro-p">Hi, I'm Menno Engels, a Software Engineer who is passionate about creating
+                        practical solutions to real world problems.
+                    </p>
+                    <p id="intro_2" class="intro-p">I am currently focusing on building interesting and practical projects to further
+                        develop my Python skills.</p>
+                </div>
+            </div>
+            <div class="experience">
+                <h4 id="experience_title">Experience</h4>
+                <div class="content">
+                    <div class="exp-item">
+                        <div class="job">
+                            <a id="experience_1_company" class="company" href="" target="_blank">In-terest</a>
+                            <div id="experience_1_duration" class="duration">Februari 2021 &mdash; June 2022</div>
+                        </div>
+                        <div id="experience_1_title" class="title">IOS Developer</div>
+                        <ul class="description">
+                            <li id="experience_1_description_1">As part of the development team for an indoor positioning system, I played a key role in
+                                rewriting code for an IOS App to improve its performance</li>
+                            <li id="experience_1_description_2">Developed code for an outdated content management system and integrating it with the
+                                mobile app.</li>
+                            <li id="experience_1_description_3">Guiding new interns in learning Swift and continuing the development of an IOS
+                                application</li>
+                        </ul>
+                    </div>
+                    <div class="exp-item">
+                        <div class="job">
+                            <a id="experience_2_company" class="company" href="https://www.hcas.nl" target="_blank">HCAS Hockey Club.</a>
+                            <div id="experience_2_duration" class="duration">September 2019 &mdash; Februari 2020</div>
+                        </div>
+                        <div id="experience_2_title" class="title">Full-Stack Developer</div>
+                        <ul class="description">
+                            <li id="experience_2_description_1">Developed an online platform to manage hockey players and enhance their performance.</li>
+                            <li id="experience_2_description_2">Build a training environment that enables coaches to design and
+                                implement personalized training programs.</li>
+                            <li id="experience_2_description_3">In collaboration with SmartGoals B.V., hockey players were enabled to rewatch their
+                                matches and training sessions online.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="right">
+            <div class="data content">
+                <div>Menno Engels</div>
+                <div>Menno.engels@live.nl</div>
+                <div>MennoEngels.com</div>
+                <div>+31652379121</div>
+            </div>
+            <div class="education">
+                <h4 id="education_title">Education</h4>
+                <div class="content">
+                    <div class="school"><a id="education_school" href="https://www.summacollege.nl" target="_blank">Summa College</a>
+                    </div>
+                    <div class="college"><a id="education_college" href="https://www.summacollege.nl/opleidingen/software-developer-bol"
+                            target="_blank">ICT</a></div>
+                    <div class="major"><strong id="major_title">Major:</strong><span id="major"> Software Development</span></div>
+                    <div class="minor"><strong id="minor_title">Minor:</strong><span id="minor"> Mobile Application Development</span></div>
+                    <div id="education_graduation" class="grad-date">Graduated in Juli 2021</div>
+                </div>
+            </div>
+            <div class="skills">
+                <h4 id="skill_title">Skills</h4>
+                <div class="content">
+                    <div class="skill-category">
+                        <strong id="skill_title_1">Languages: &nbsp;</strong>
+                        <span id="skill_1">Python, HTML, CSS, JavaScript, PHP, Swift, C#</span>
+                    </div>
+                    <div class="skill-category">
+                        <strong id="skill_title_2">Libraries &amp; Frameworks: &nbsp;</strong>
+                        <span id="skill_2">Flask, Pandas, Matplotlib, </span>
+                    </div>
+                    <div class="skill-category">
+                        <strong id="skill_title_3">Tools: &nbsp;</strong>
+                        <span id="skill_3">Git, VSCode, Xcode, MySQLWorkbench, Excel, Jupyter</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
